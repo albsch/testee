f749f3540cb1099c028c174238b804860c7b5fbc
Albert Schimpf <git@schimpf.link>
2023-03-14 15:55:50 +0100

    tes

--- main.ts
+++ main.ts
@@ -793,9 +793,10 @@ namespace grove {
     /**
      * Send data to api
      */
-    //% block="Send Data to your Api Channel|Write API Key %apiKey|Field1 %field1|Field2 %field2|Field3 %field3|Field4 %field4|Field5 %field5|Field6 %field6|Field7 %field7|Field8 %field8"
+    //% block="Send Data to your Api Channel|Write API Key %apiKey|URL %apiUrl|Field1 %field1|Field2 %field2|Field3 %field3|Field4 %field4|Field5 %field5|Field6 %field6|Field7 %field7|Field8 %field8"
     //% group="UartWiFi"
     //% apiKey.defl="your Write API URLLLL"
+    //% apiUrl.defl="your Write API URLLLL"
     export function sendToApi(apiKey: string, apiUrl:string, apiPort:string, field1: number, field2: number, field3: number, field4: number, field5: number, field6: number, field7: number, field8: number) {
         let result = 0
         let retry = 2

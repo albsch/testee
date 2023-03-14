70cd6581b570749c8b0f37661e8d2caf19100087
Albert Schimpf <git@schimpf.link>
2023-03-14 15:52:54 +0100

    test

--- main.ts
+++ main.ts
@@ -791,6 +791,53 @@ namespace grove {
     }
 
     /**
+     * Send data to api
+     */
+    //% block="Send Data to your Api Channel|Write API Key %apiKey|Field1 %field1|Field2 %field2|Field3 %field3|Field4 %field4|Field5 %field5|Field6 %field6|Field7 %field7|Field8 %field8"
+    //% group="UartWiFi"
+    //% apiKey.defl="your Write API URLLLL"
+    export function sendToApi(apiKey: string, field1: number, field2: number, field3: number, field4: number, field5: number, field6: number, field7: number, field8: number) {
+        let result = 0
+        let retry = 2
+
+        // close the previous TCP connection
+        if (isWifiConnected) {
+            sendAtCmd("AT+CIPCLOSE")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+        }
+
+        while (isWifiConnected && retry > 0) {
+            retry = retry - 1;
+            // establish TCP connection
+            sendAtCmd("AT+CIPSTART=\"TCP\",\""+apiKey+"\",8080")
+            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
+            if (result == 3) continue
+
+            let data = "GET /update?api_key=" + apiKey
+            if (!isNaN(field1)) data = data + "&field1=" + field1
+            if (!isNaN(field2)) data = data + "&field2=" + field2
+            if (!isNaN(field3)) data = data + "&field3=" + field3
+            if (!isNaN(field4)) data = data + "&field4=" + field4
+            if (!isNaN(field5)) data = data + "&field5=" + field5
+            if (!isNaN(field6)) data = data + "&field6=" + field6
+            if (!isNaN(field7)) data = data + "&field7=" + field7
+            if (!isNaN(field8)) data = data + "&field8=" + field8
+
+            sendAtCmd("AT+CIPSEND=" + (data.length + 2))
+            result = waitAtResponse(">", "OK", "ERROR", 2000)
+            if (result == 3) continue
+            sendAtCmd(data)
+            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
+
+            // // close the TCP connection
+            // sendAtCmd("AT+CIPCLOSE")
+            // waitAtResponse("OK", "ERROR", "None", 2000)
+
+            if (result == 1) break
+        }
+    }
+
+    /**
      * Send data to IFTTT
      */
     //% block="Send Data to your IFTTT Event|Event %event|Key %key|value1 %value1|value2 %value2|value3 %value3"

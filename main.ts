dc0f13c4d293fc457558c37a608013446b602880
Albert Schimpf <git@schimpf.link>
2023-03-14 15:43:43 +0100

    back to old

--- main.ts
+++ main.ts
@@ -792,6 +792,60 @@ namespace grove {
         }
     }
 
+    /**
+     * Send data to IFTTT
+     */
+    //% block="Send Data to your IFTTT Event|Event %event|Key %key|value1 %value1|value2 %value2|value3 %value3"
+    //% group="UartWiFi"
+    //% event.defl="your Event"
+    //% key.defl="your Key"
+    //% value1.defl="hello"
+    //% value2.defl="micro"
+    //% value3.defl="bit"
+    export function sendToIFTTT(event: string, key: string, value1: string, value2: string, value3: string) {
+        let result = 0
+        let retry = 2
+
+        // close the previous TCP connection
+        if (isWifiConnected) {
+            sendAtCmd("AT+CIPCLOSE")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+        }
+
+        while (isWifiConnected && retry > 0) {
+            retry = retry - 1;
+            // establish TCP connection
+            sendAtCmd("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80")
+            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
+            if (result == 3) continue
+
+            let data = "GET /trigger/" + event + "/with/key/" + key
+            data = data + "?value1=" + value1
+            data = data + "&value2=" + value2
+            data = data + "&value3=" + value3
+            data = data + " HTTP/1.1"
+            data = data + "\u000D\u000A"
+            data = data + "User-Agent: curl/7.58.0"
+            data = data + "\u000D\u000A"
+            data = data + "Host: maker.ifttt.com"
+            data = data + "\u000D\u000A"
+            data = data + "Accept: */*"
+            data = data + "\u000D\u000A"
+
+            sendAtCmd("AT+CIPSEND=" + (data.length + 2))
+            result = waitAtResponse(">", "OK", "ERROR", 2000)
+            if (result == 3) continue
+            sendAtCmd(data)
+            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
+
+            // // close the TCP connection
+            // sendAtCmd("AT+CIPCLOSE")
+            // waitAtResponse("OK", "ERROR", "None", 2000)
+
+            if (result == 1) break
+        }
+    }
+
 
     function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
         let buffer = ""
